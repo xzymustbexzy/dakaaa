00763dba27feeffd42b4a3c5ddda2d46c9af699c
Ziyang Xiao <xiaoziyang.xzy@gmail.com>
2022-08-21 15:55:41 +0800

    升级到了新版selenium的接口

--- daka.py
+++ daka.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
@@ -27,6 +27,12 @@ question_choice = {
     'sfcxzysx': '否 No', # 今日是否有涉及涉疫情的管控措施
     'sfjcbh': '否 No', # 是否有与新冠疫情确诊人员或密接人员有接触的情况? 
 }
+
+location = {
+    'long': 120.12445439312742,
+    'lat': 30.26638054406842
+}
+
 
 
 def synchronize_trial(delay, max_trial=10, msg=''):
@@ -50,26 +56,28 @@ def synchronize_trial(delay, max_trial=10, msg=''):
 
 @synchronize_trial(delay=0.5, msg='未找到用户名和密码输入框，正在重试')
 def login(username, password):
-    username_box = driver.find_element_by_id('username')
+    username_box = driver.find_element('id', 'username')
     username_box.send_keys(username)
-    password_box = driver.find_element_by_id('password')
+    password_box = driver.find_element('id', 'password')
     password_box.send_keys(password)
-    login_btn = driver.find_element_by_id('dl')
+    login_btn = driver.find_element('id', 'dl')
     login_btn.click()
 
 
 @synchronize_trial(delay=0.5, msg='未找到对应选项，正在重试')
 def fill_form():
+    print('开始填选选项...')
     for div_name, choice in question_choice.items():
-        div = driver.find_element_by_name(div_name)
-        btn = div.find_element_by_xpath(f"./div/div/span[text()='{choice}']")
+        div = driver.find_element('name', div_name)
+        btn = div.find_element('xpath', f"./div/div/span[text()='{choice}']")
         btn.click()
         time.sleep(1)
-    place_span = driver.find_element_by_name('area')
+    place_span = driver.find_element('name', 'area')
     place_span.click()
     time.sleep(1)
-    agree_div = driver.find_element_by_name('sfqrxxss')
-    agree_div.click()
+    print('准备提交...')
+    # agree_div = driver.find_element('name', 'sfqrxxss')
+    # agree_div.click()
 
 
 def do_daka(username, password, delay=False):
@@ -80,16 +88,32 @@ def do_daka(username, password, delay=False):
         
     global driver
     driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
+    driver.execute_cdp_cmd(
+        "Browser.grantPermissions",  # 授权地理位置信息
+        {
+            "origin": "https://healthreport.zju.edu.cn/",
+            "permissions": ["geolocation"]
+        },
+    )
+    driver.execute_cdp_cmd(
+        "Emulation.setGeolocationOverride",  # 虚拟位置
+        {
+            "latitude": location['lat'],
+            "longitude": location['long'],
+            "accuracy": 50,
+        },
+    )
+    print(driver)
     driver.get(url)
     print('开始登录...')
     login(username, password)
     print(f'登录成功，账号{username}')
     fill_form()
     print('表单填写完成，准备提交信息...')
-    submit_btn = driver.find_element_by_xpath("//*[contains(text(), '提交信息')]")
+    submit_btn = driver.find_element('xpath', "//*[contains(text(), '提交信息')]")
     submit_btn.click()
     time.sleep(2)
-    comfirm_btn = driver.find_element_by_css_selector('div.wapcf-btn.wapcf-btn-ok')
+    comfirm_btn = driver.find_element('css selector', 'div.wapcf-btn.wapcf-btn-ok')
     comfirm_btn.click()
     print('打卡信息提交成功!!')
 
@@ -97,3 +121,6 @@ def do_daka(username, password, delay=False):
     dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
     print(f'✅ 打卡成功，用户: {username}, 打卡时间: {dt_string}')
 
+
+if __name__ == '__main__':
+    do_daka('22021206', 'zju244712')
